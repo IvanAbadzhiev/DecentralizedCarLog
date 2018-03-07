ad3a894cc4f813fef8c12704167a03098131c3b5
ivan3a <email9410@abv.bg>
2018-03-07 18:12:38 +0200

    add AddServiceTOCar functionallity

--- contracts/CarLog.sol
+++ contracts/CarLog.sol
@@ -1,47 +1,57 @@
 pragma solidity ^0.4.18;
 
-contract CarLog  {
-    
+contract CarLog {
     struct Car {
         string vin;
         address owner;
     }
     
-    struct Owner {
-        address owner;
-        Car[] cars;
-    }
-    
-    mapping(address => mapping (string => Car)) cars;
-    mapping(string => address) vinToAddress;
-    mapping(address => Car[]) carToOwners;
+    mapping(address => Car[]) private cars;
+    mapping(string => Car) private carVins;
+    mapping(string => address[]) private carToService;
     
     address private contractOwner;
+  
+    modifier onlyCarOwner(string _vin){
+        require(carVins[_vin].owner == msg.sender);
+        _;
+    }
     
     function CarLog() public{
         contractOwner = msg.sender;
     }
     
-    function addCar(string _vin){
-        cars[msg.sender][_vin] =  Car(_vin, msg.sender);
-        vinToAddress[_vin] = msg.sender;
-        carToOwners[msg.sender].push(Car(_vin, msg.sender));
-    }
-    
-    function getCar(string _vin) public view returns(string){
-        return cars[vinToAddress[_vin]][_vin].vin;
-    }
-    
-    function getCarsByOwner() public view returns( bytes32[] ) {
-        bytes32[] vins;
-        
-        for(uint i = 0; i < carToOwners[msg.sender].length; i++){
-            vins.push(stringToBytes32(carToOwners[msg.sender][i].vin));
+   function addAcar(string _vin) public {
+       cars[msg.sender].push(Car(_vin, msg.sender));
+       carVins[_vin] = Car(_vin, msg.sender);
+   }
+   
+   function getCar(string _vin) public view returns(string){
+       return carVins[_vin].vin;
+   }
+   
+   function addVehicleService(string _vin, address _address) public onlyCarOwner(_vin) {
+       carToService[_vin].push(_address);
+   }
+   
+   // TODO validation.
+   // check is service already added
+   // check address is not car owner
+   function getVehicleServices(string _vin) public view onlyCarOwner(_vin) returns(address[]) {
+       return carToService[_vin];
+   }
+   
+   function getMyCars() public view returns(bytes32[]) {
+       bytes32[] storage vins;
+       
+        for(uint i = 0; i < cars[msg.sender].length; i++){
+            vins.push(stringToBytes32(cars[msg.sender][i].vin));
         }
         
         return vins;
-    }
-    
+   }
+   
+ 
     function stringToBytes32(string memory source) public view returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
